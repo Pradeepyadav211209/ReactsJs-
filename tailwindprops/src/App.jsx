2b6f08e7e6d867766779b1c349a747b9f00214f0
pradeep yadav <pradeepyadav211209@gmail.com>
2026-09-13 14:48:12 +0530

    Props + Tailwind

---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+ 
+
+  return (
+    <>
+         <Card username="Alice" price="1000"/>
+         <Card username="Bob" price="1500"/>
+         <Card username="Charlie"/>
+    </>
+  )
+}
+
+export default App
